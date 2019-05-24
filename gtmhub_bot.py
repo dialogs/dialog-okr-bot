3697ebb4c1fd02ba573439204f6cd4f9d31f5103
willy2dg <48071439+willy2dg@users.noreply.github.com>
2019-05-24 14:32:16 +0200

    Raise exceptions on initialization
    Raise exceptions on initialization for debuggin purposes

--- gtmhub_bot.py
+++ gtmhub_bot.py
@@ -190,3 +190,4 @@ if __name__ == '__main__':
         bot.messaging.on_message(on_message, on_interact)
     except Exception as ex:
         print('Error initializing the bot - '+str(ex))
+        raise ex
